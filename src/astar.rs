dce3e41c97fde7db74c918ceaa573cd8e9892f46
Peter Eliah Kodl <peter.e.kodl@gmail.com>
2021-06-21 19:50:07 +0200

    Initial commit.

---
+++ src/astar.rs
@@ -0,0 +1,255 @@
+extern crate rayon;
+use rayon::prelude::*;
+
+#[derive(Clone, Copy, PartialEq)]
+pub struct Position
+{
+
+    pub x: isize,
+    pub y: isize
+
+}
+
+impl std::ops::Add for Position
+{
+    type Output = Self;
+    fn add(self, other: Self) -> Self
+    {
+        Self{x: self.x + other.x, y: self.y + other.y}
+    }
+}
+
+#[derive(Clone,Copy)]
+struct Node
+{
+    pub pos:Position,
+    pub parent_index:Option<usize>,
+    pub g: usize,
+    pub h: usize,
+    pub open:bool
+}
+
+impl Node
+{
+    fn new(pos: Position, parent_index: Option<usize>, g: usize, h: usize) -> Self
+    {
+        Self
+        {
+            pos,
+            parent_index,
+            g,
+            h,
+            open: true
+        }
+    }
+    fn f(&self) -> usize
+    {
+        self.g + self.h
+    }
+
+}
+
+impl PartialEq for Node
+{
+    fn eq(&self, other: &Self) -> bool
+    {
+        self.f() == other.f()
+    }
+}
+
+impl Eq for Node{}
+
+impl PartialOrd for Node
+{
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering>
+    {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Node
+{
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering
+    {
+        (self.f()).cmp(&other.f())       
+    }
+}
+
+#[derive(Clone, Copy)]
+pub enum Cell
+{
+    Start,
+    Goal,
+    Wall,
+    Free,
+    Path,
+    Visited
+
+}
+
+pub struct Grid
+{
+    memory: std::vec::Vec<Cell>, 
+    pub width: usize,
+    pub height: usize
+}
+
+impl Grid
+{
+
+    pub fn new(width: usize, height: usize) -> Self
+    {
+        let mut memory: Vec<Cell> = std::vec::Vec::with_capacity(width*height);
+        memory.resize(width*height, Cell::Free);
+        Self {memory, width, height}
+    }
+
+    pub fn get_cell(&self, x: usize, y: usize) -> Option<Cell>
+    {
+        if x >= self.width || y >= self.height
+        {
+            return None;
+        }
+        Some(self.memory[x + y*self.width])
+    }
+
+    pub fn set_cell(&mut self, x: usize, y:usize, value:Cell)
+    {
+        self.memory[x + y*self.width] = value;
+    }
+
+    // Clears the entire grid
+    pub fn clear(&mut self)
+    {
+        self.memory.fill(Cell::Free);
+    }
+
+    // Clears path and visited cells
+    fn clear_path(&mut self)
+    {
+        for x in self.memory.iter_mut()
+        {
+            match *x
+            {
+                Cell::Path | Cell::Visited => *x = Cell::Free,
+                _ => {}
+            }
+        }
+    }
+
+    
+
+    fn solve(&mut self, start_pos: Option<Position>, goal_pos: Option<Position>) -> (Option<Vec<Position>>, Option<Vec<Node>>)
+    {
+        if start_pos.is_none() || start_pos.is_none()
+        {
+            return (None, None);
+        }
+        self.clear_path();
+        let start_pos = start_pos.unwrap();
+        let goal_pos = goal_pos.unwrap();
+
+        let heuristic = |a:&Position, b:&Position| ((a.x - b.x).pow(2) + (a.y-b.y).pow(2)) as usize; 
+        
+        
+        let mut node_list = Vec::new();
+
+        node_list.push(Node::new(start_pos, None, 0, heuristic(&start_pos, &goal_pos)));
+        
+        loop
+        {
+            // Get the smallest node from the open list, if there is none, the function returns
+            // None
+            let current_node_index: usize = match node_list.par_iter().enumerate().filter(|x| x.1.open).min_by(|a, b| a.1.cmp(b.1))
+            {
+                Some((value, _)) => value,
+                None => return (None, None)
+            };
+           
+            node_list[current_node_index].open = false;
+
+            let mut offset = Position{x:-1, y:0};
+
+            for _ in 0..4
+            {
+                std::mem::swap(&mut offset.y, &mut offset.x);
+                offset.y = -offset.y;
+
+                let node_pos = node_list[current_node_index].pos + offset;
+                let mut node = Node::new(node_pos, Some(current_node_index), node_list[current_node_index].g + 1, heuristic(&node_pos, &goal_pos));
+                if let Some(Cell::Wall) | None = self.get_cell(node_pos.x as usize, node_pos.y as usize)
+                {
+                    continue;
+                }
+
+                if node.pos == goal_pos
+                {
+                    let mut path = Vec::new();
+                    loop
+                    {
+                        path.push(node.pos);
+                        match node.parent_index
+                        {
+                            Some(index) => node = node_list[index],
+                            None => return (Some(path), Some(node_list))
+                        }
+                    }
+                }
+                
+                // Check if the node is in the open list.
+                let identical_node = node_list.par_iter_mut().find_any(|x| x.pos == node.pos); 
+
+                match identical_node
+                {
+                    Some(value) => 
+                    {
+                        if node.g < value.g && value.open
+                        {
+                            value.g = node.g;
+                            value.parent_index = node.parent_index;
+                        }
+                    },
+                    None => 
+                    {
+                        node_list.push(node);
+                    }
+                }
+
+            }
+        }
+    }
+
+    pub fn create_path(&mut self, start_pos: Option<Position>, goal_pos: Option<Position>)
+    {
+        let solution = self.solve(start_pos, goal_pos);
+        if let Some(visited) = solution.1
+        {
+            for node in visited
+            {
+                if let Some(Cell::Free) = self.get_cell(node.pos.x as usize, node.pos.y as usize)
+                {
+                    self.set_cell(node.pos.x as usize, node.pos.y as usize, Cell::Visited);
+                }
+            }
+        }
+        if let Some(path) = solution.0
+        {
+            for pos in path
+            {
+                match self.get_cell(pos.x as usize, pos.y as usize)
+                {
+                    Some(Cell::Free) | Some(Cell::Visited) => self.set_cell(pos.x as usize, pos.y as usize, Cell::Path),
+                    _ => {}
+                }
+            }
+        }
+        
+        
+    }
+
+    
+}
+
+
+
+
---
+++ src/main.rs
@@ -0,0 +1,150 @@
+extern crate rustbox;
+
+use rustbox::*;
+mod astar; use astar::*;
+
+enum Mode
+{
+    Start,
+    Goal,
+    Wall,
+    Free
+}
+
+
+
+struct Data
+{
+    pub grid: Grid,
+    pub mode: Mode,
+    pub start_pos: Option<Position>,
+    pub goal_pos: Option<Position>
+
+}
+
+fn draw_grid(grid: &Grid, rustbox: &RustBox)
+{
+    for y in 0..grid.height
+    {
+        for x in 0..grid.width
+        {
+            match grid.get_cell(x, y).unwrap() 
+            {
+                Cell::Start => 
+                {
+                    rustbox.print_char(x, y, RB_BOLD, Color::Red, Color::Default, '@');
+                },
+                Cell::Goal =>
+                {
+                    rustbox.print_char(x, y, RB_BOLD, Color::Green, Color::Default, 'X'); 
+                },
+                Cell::Wall =>
+                {
+                    rustbox.print_char(x, y, RB_NORMAL, Color::White, Color::Default, '#');
+                },
+                Cell::Free => 
+                {
+                    rustbox.print_char(x, y, RB_NORMAL, Color::White, Color::Default, '.');
+                },
+                Cell::Path =>
+                {
+                    rustbox.print_char(x, y, RB_BOLD, Color::Yellow, Color::Default, '.');
+                },
+                Cell::Visited =>
+                {
+                    rustbox.print_char(x, y, RB_NORMAL, Color::Cyan, Color::Default, '.');
+                }
+            }
+        }
+    }
+}
+
+fn draw(rustbox: &RustBox, data: &Data)
+{
+    draw_grid(&data.grid, &rustbox);
+    let mode:&str = match data.mode
+    {
+        Mode::Wall => "Wall",
+        Mode::Free => "Free",
+        Mode::Goal => "Goal",
+        Mode::Start => "Start"
+    };
+    rustbox.print(0, 0, RB_REVERSE, Color::White, Color::Default, 
+                  format!("[{}] | w Wall | f Free | g Goal | s Start | c Clear | p Solve | q Quit", mode).as_str());
+}
+
+fn handle_mouse(data: &mut Data, x: usize, y: usize)
+{
+    match data.mode
+    {
+        Mode::Free => data.grid.set_cell(x, y, Cell::Free),
+        Mode::Wall => data.grid.set_cell(x, y, Cell::Wall),
+        Mode::Goal => 
+        {
+            match data.goal_pos
+            {
+                Option::Some(pos) =>
+                {
+                    data.grid.set_cell(pos.x as usize, pos.y as usize, Cell::Free);
+                },
+                Option::None => {}
+            }
+            data.goal_pos = Some(Position{x: x as isize, y: y as isize});
+            data.grid.set_cell(x, y, Cell::Goal);
+
+        },
+        Mode::Start =>
+        {
+            match data.start_pos
+            {
+                Option::Some(pos) =>
+                {
+                    data.grid.set_cell(pos.x as usize, pos.y as usize, Cell::Free);
+                },
+                Option::None => {}
+            }
+            data.start_pos = Some(Position{x: x as isize, y: y as isize});
+            data.grid.set_cell(x, y, Cell::Start); 
+        }
+
+    }
+}
+fn main() {
+    let rustbox = RustBox::init(InitOptions::default()).unwrap();
+    rustbox.set_input_mode(InputMode::EscMouse); 
+    let mut data = Data 
+    {
+        mode: Mode::Wall,
+        grid: Grid::new(rustbox.width(), rustbox.height()),
+        start_pos: Option::None,
+        goal_pos: Option::None
+    };
+
+    loop
+    {
+        draw(&rustbox, &data);
+        rustbox.present();
+        match rustbox.poll_event(false).unwrap()
+        {
+            Event::KeyEvent(c) => 
+            {
+                match c
+                {
+                    Key::Char('q') => break,
+                    Key::Char('w') => data.mode = Mode::Wall,
+                    Key::Char('f') => data.mode = Mode::Free,
+                    Key::Char('g') => data.mode = Mode::Goal,
+                    Key::Char('s') => data.mode = Mode::Start,
+                    Key::Char('c') => data.grid.clear(),
+                    Key::Char('p') => data.grid.create_path(data.start_pos, data.goal_pos),
+                    _ => {}
+                }
+            },
+            Event::MouseEvent(Mouse::Left, x, y) =>
+            {
+                handle_mouse(&mut data, x as usize, y as usize)
+            },
+            _ => {}
+        }
+    }
+}
